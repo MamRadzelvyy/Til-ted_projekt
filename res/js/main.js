86a2c755dc4cf9c39f60dfd315145f0fbec794da
Your Namec <MamRadZelvyy>
2023-12-15 09:19:44 +0100

    Till-ted

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,15 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d");
+const battleBus = new Image();
+battleBus.src = "./res/img/boss/battle_bus.png";
+
+let bossPaths = [
+"./res/img/boss/battle_bus.png",
+ "./res/img/boss/BOSS.png",
+ "./res/img/boss/fnkid.png",
+ "./res/img/boss/kevin.png",
+ "./res/img/boss/mecha.png",
+];
 
 canvas .width = 1280;
 canvas .height = 720;
@@ -47,3 +57,44 @@ ctx.lineTo(580, 98);
 ctx.lineTo(620, 98);
 ctx.lineTo(640, 85);
 ctx.stroke()
+
+
+let bus = {
+    x: 200,
+    y: 200,
+    xVelocity: 1,
+    yVelocity: 1,
+}
+
+
+
+window.onload = () => {
+  setInterval(() => {
+
+if (canvas.width <= bus.x + 200){
+    bus.xVelocity *= -1;
+battleBus.src = bossPaths[1];
+
+}
+if (bus.x <= 0){
+    bus.xVelocity *= -1;
+    battleBus.src = bossPaths[3];
+}
+if (canvas.height <= bus.y + 200){
+    bus.yVelocity *= -1;
+    battleBus.src = bossPaths[2];
+}
+    if (bus.y <= 0){
+        bus.yVelocity *= -1;
+        battleBus.src = bossPaths[4];
+    }
+
+
+    bus.x += bus.xVelocity;
+    bus.y += bus.yVelocity;
+    ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, 1280, 720);
+  ctx.drawImage(battleBus, bus.x, bus.y, 200, 200);
+  }, 10); 
+}
+
